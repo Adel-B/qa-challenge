5ee1185c47e0d85364e7d00d4d8f128d642a699c
Adel Bensaad <bensaad.adel@gmail.com>
2021-06-15 08:37:13 +0200

    ci(performance): add lighthouse to Audit performance

---
+++ .lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['http://localhost:3000/'],
+        startServerCommand: 'yarn start',
+      },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+  };
+  
